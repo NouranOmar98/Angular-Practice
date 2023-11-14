b76faaafeb3c3d63e03a7d1045e67e4aabf2ab2e
Nouran Kanani <n.Kanani.ext@Tahaluf.ae>
2023-11-14 19:59:48 +0300

    second commit

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+  styleUrls: ['./property-list.component.css'],
 })
-export class PropertyListComponent {
-
+export class PropertyListComponent implements OnInit {
+  properties: Array<any> = [
+    {
+      id: 1,
+      Name: 'Birla House',
+      Type: 'House',
+      Price: 12000,
+    },
+    {
+      id: 2,
+      Name: 'Birla House',
+      Type: 'House',
+      Price: 12000,
+    },
+    {
+      id: 3,
+      Name: 'Birla House',
+      Type: 'House',
+      Price: 12000,
+    },
+  ];
+  constructor() {}
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
 }
